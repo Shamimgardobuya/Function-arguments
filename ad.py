e58c8ed1236bc9980ab2c85fabb7a17e144d395a
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-20 23:30:10 +0300

    flexible functions with *args

---
+++ ad.py
@@ -0,0 +1,20 @@
+
+
+def add(numbers):
+   p=sum(numbers)
+   print(p)
+add([2,4,5,6,7,7])
+def addi(*numb):
+    y=0
+    for n in numb:
+        y+=n
+    return y
+# # def mult(numpy):   pseudocode
+#    define function and give parameter name
+#           for n in name
+#              initialize z and assign one 
+#                 z*=n
+#              multiply n and assign it to z    mult*=n 
+#              call the function and give it any parameter.
+# #     z=1
+# #     for
